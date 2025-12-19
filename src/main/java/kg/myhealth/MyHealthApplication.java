ed8e29f0ce0cffdd3290752320ccfb71c1777be1
Dastan <tu.dastan.tu@gmail.com>
2025-12-19 12:40:35 +0600

    chore: application entry point

---
+++ src/main/java/kg/myhealth/MyHealthApplication.java
@@ -0,0 +1,11 @@
+package kg.myhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyHealthApplication.class, args);
+    }
+}
